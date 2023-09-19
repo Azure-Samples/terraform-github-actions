bdf58a073600052430344b73da29be6a92e804d1
KirylY <Kiryl.Yaroshkin@ahold.com>
2023-09-19 16:30:34 +0300

    Added appi

---
+++ appinsights.tf
@@ -0,0 +1,7 @@
+resource "azurerm_application_insights" "appi" {
+  name                = var.appi_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.name
+  //workspace_id        = data.azurerm_log_analytics_workspace.la_frmd.id
+  application_type    = "web"
+}
--- variables.tf
+++ variables.tf
@@ -1,2 +1,3 @@
 variable "resource_group_name" {}
 variable "location" {}
+variable "appi_name" {}
