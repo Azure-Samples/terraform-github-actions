bf88f8f961037104a4d80a1ee2b6cf2ae114120f
rjohnson318 <8972701+rjohnson318@users.noreply.github.com>
2023-09-30 18:17:39 -0500

    Update main.tf
    updated the storage account name

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   # Update this block with the location of your terraform state file
   backend "azurerm" {
     resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
+    storage_account_name = "terraformgithubactions12"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
