1389ff490218e343d813242bab1254b727a1f349
Matthew White <74813866+MattWhite-personal@users.noreply.github.com>
2024-07-04 11:30:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ provider "azurerm" {
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
-resource "azurerm_resource_group" "rg-aks" {
+resource "azurerm_resource_group" "rg-aks2" {
   name     = var.resource_group_name
   location = var.location
 }
