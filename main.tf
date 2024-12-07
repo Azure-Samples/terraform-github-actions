4ad5b9ddd7ca993ff923b5fc8b1fcd30aa996a73
Vibhor Meharia <vibhormeharia@gmail.com>
2024-12-07 12:05:49 +0530

    change in the azure backend

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
+    resource_group_name  = "cloud-shell-storage-eastus"
+    storage_account_name = "cs210032000df9b6dd3"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
